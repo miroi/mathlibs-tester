17c4c5965e99224c74262bfbce018844abdbdd00
Miro ILIAS <Miroslav.Ilias@umb.sk>
2015-08-10 16:46:57 +0200

    upd

--- cmake/custom/test.cmake
+++ cmake/custom/test.cmake
@@ -1,16 +1,5 @@
-message("here custom  CTest:")
 include(CTest)
-message("CTest: CTEST_PROJECT_NAME=${CTEST_PROJECT_NAME}")
 enable_testing()
 
-message("CMAKE_SOURCE_DIR=${CMAKE_SOURCE_DIR}")
-message("PROJECT_BINARY_DIR=${PROJECT_BINARY_DIR}")
-message("EXECUTABLE_OUTPUT_PATH=${EXECUTABLE_OUTPUT_PATH}")
-
-#include()
-
- #   add_executable(example1 example.F90)
- #   target_link_libraries(example1 ${MATH_LIBS})
-
+# this target is compiled before, see src/CMakeFiles.txt
 add_test(t ${PROJECT_BINARY_DIR}/bin/example)
-#add_test(example1 ${PROJECT_BINARY_DIR}/bin/example1)
