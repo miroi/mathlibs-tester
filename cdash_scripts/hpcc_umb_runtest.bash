34a9f3ee5e20f0ec25e0a8a688401e4bbca69537
Miro ILIAS <Miroslav.Ilias@umb.sk>
2015-08-10 12:04:03 +0200

    some fixes

--- cdash_scripts/hpcc_umb_runtest.bash
+++ cdash_scripts/hpcc_umb_runtest.bash
@@ -3,7 +3,14 @@
 echo "hello !"
 pwd
 
-#python ./setup.py --help
+cd tmp
+if [[ -d "mathlibs-tester" ]]; then
+  echo "deleting previous clone mathlibs-tester"
+  /bin/rm -rf mathlibs-tester
+fi
+git clone git@github.com:miroi/mathlibs-tester.git
+
+cd  mathlibs-tester
 
 BUILD=build_gfortran
 if [[ -d "$BUILD" ]]; then
@@ -13,6 +20,7 @@ fi
 ./setup --fc=gfortran  $BUILD
 cd $BUILD
 make 
+ls -lt bin/example
 ldd bin/example
 bin/example
 
