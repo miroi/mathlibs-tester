23370a8eadbd288cda7c2b4ba394257547a418a3
Miro ILIAS <Miroslav.Ilias@umb.sk>
2015-09-01 11:41:34 +0200

    update test

--- cmake/custom/test.cmake
+++ cmake/custom/test.cmake
@@ -2,4 +2,6 @@ include(CTest)
 enable_testing()
 
 # this target is compiled before, see src/CMakeFiles.txt
+add_test(test_lapack_blas ${PROJECT_BINARY_DIR}/bin/example 50 2)
 add_test(test_lapack_blas ${PROJECT_BINARY_DIR}/bin/example 100 2)
+add_test(test_clapack_cblas ${PROJECT_BINARY_DIR}/bin/example_c)
