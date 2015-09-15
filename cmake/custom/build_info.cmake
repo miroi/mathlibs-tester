873cc20ef6e1bf8bd597d90c87bc6420b181a27f
Miro ILIAS <Miroslav.Ilias@umb.sk>
2015-09-15 15:54:03 +0200

    more CMake output

--- cmake/custom/build_info.cmake
+++ cmake/custom/build_info.cmake
@@ -12,7 +12,7 @@ set(_host                   "unknown")
 set(_system                 "unknown")
 if (CMAKE_SYSTEM)
     set(_system ${CMAKE_SYSTEM})
-    message("-- System                   : ${CMAKE_SYSTEM}")
+    message("-- Operating system         : ${CMAKE_SYSTEM}")
 endif()
 
 set(_system_name            "unknown")
@@ -21,13 +21,11 @@ if (CMAKE_SYSTEM_NAME)
     #message("-- System name              : ${CMAKE_SYSTEM_NAME}")
 endif()
 
-
 set(_cmake_version          "unknown")
 if (CMAKE_VERSION)
     set(_cmake_version     ${CMAKE_VERSION})
     message("-- CMake version            : ${CMAKE_VERSION}")
 endif()
-
 
 set(_cmake_generator       "unknown")
 if (CMAKE_GENERATOR)
@@ -41,7 +39,6 @@ if (CMAKE_BUILD_TYPE)
     message("-- CMake build type         : ${CMAKE_BUILD_TYPE}")
 endif()
 
-
 set(_fortran_compiler_id       "unknown")
 if (CMAKE_Fortran_COMPILER_ID)
     set(_fortran_compiler_id ${CMAKE_Fortran_COMPILER_ID})
@@ -52,6 +49,12 @@ set(_fortran_compiler       "unknown")
 if (CMAKE_Fortran_COMPILER)
     set(_fortran_compiler ${CMAKE_Fortran_COMPILER})
     message("-- Fortran compiler         : ${CMAKE_Fortran_COMPILER}")
+endif()
+
+set(_python_fortran_version "unknown")
+if (PYTHON_Fortran_VERSION)
+    set(_python_fortran_version ${PYTHON_Fortran_VERSION})
+    message("-- Fortran compiler version : ${PYTHON_Fortran_VERSION}")
 endif()
 
 set(_fortran_compiler_flags "unknown")
@@ -72,10 +75,22 @@ if (CMAKE_C_COMPILER)
     message("-- C compiler               : ${CMAKE_C_COMPILER}")
 endif()
 
+set(_python_c_version "unknown")
+if (PYTHON_C_VERSION)
+    set(_python_c_version ${PYTHON_C_VERSION})
+    message("-- C compiler version       : ${PYTHON_C_VERSION}")
+endif()
+
 set(_c_compiler_flags "unknown")
 if (CMAKE_C_FLAGS)
     set(_c_compiler_flags ${CMAKE_C_FLAGS})
     message("-- C compiler flags         : ${CMAKE_C_FLAGS}")
+endif()
+
+set(_cxx_compiler_id       "unknown")
+if (CMAKE_CXX_COMPILER_ID)
+    set(_cxx_compiler_id ${CMAKE_CXX_COMPILER_ID})
+    message("-- C++ compiler ID          : ${CMAKE_CXX_COMPILER_ID}")
 endif()
 
 set(_cxx_compiler           "unknown")
@@ -84,10 +99,16 @@ if (CMAKE_CXX_COMPILER)
     message("-- C++ compiler             : ${CMAKE_CXX_COMPILER}")
 endif()
 
+set(_python_cxx_version "unknown")
+if (PYTHON_CXX_VERSION)
+    set(_python_cxx_version ${PYTHON_CXX_VERSION})
+    message("-- C++ compiler version     : ${PYTHON_CXX_VERSION}")
+endif()
+
 set(_cxx_compiler_flags     "unknown")
 if (CMAKE_CXX_FLAGS)
     set(_cxx_compiler_flags ${CMAKE_CXX_FLAGS})
-    message("-- CXX compiler flags     : ${CMAKE_CXX_FLAGS}")
+    message("-- C++ compiler flags     : ${CMAKE_CXX_FLAGS}")
 endif()
 
 set(_static_linking         "unknown")
@@ -107,7 +128,6 @@ if (EXPLICIT_LIBS)
     set(_explicit_libs ${EXPLICIT_LIBS})
     message("--     Explicit libraries   : ${EXPLICIT_LIBS}")
 endif()
-
 
 # generate the build_info.h include file with defined set of variables
 configure_file(
