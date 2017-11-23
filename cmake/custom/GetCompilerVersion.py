ed2071a7ea5f7fd44bcca8809cc8e67d23f23197
Miroslav Ilias <miro.ilias@gmail.com>
2017-11-23 21:18:09 +0100

    better fix for Python3

--- cmake/custom/GetCompilerVersion.py
+++ cmake/custom/GetCompilerVersion.py
@@ -12,7 +12,8 @@
 #
 # This script returns (as stdout) for CMake a list of 3 strings - compilers versions - separated by semicolons.
 #
-# Written by Ivan Hrasko, february 2014
+# Written by Ivan Hrasko, February 2014
+# Simplified by Miro Ilias, September 2015
 #
 
 import sys, subprocess, re
@@ -29,216 +30,79 @@ Fortran_COMPILER_VENDOR = sys.argv[6]
 #! operating system name
 CMAKE_SYSTEM_NAME = sys.argv[7]
 
+def get_compiler_version(compiler,version_flag,pattern):
+    """
+    Get compiler version based on entering aruments:
+      compiler - full path to the compiler 
+      version_flag - flag to print compiler version
+      pattern - the regular expression to extract compiler version numbers
+    """
 #! in every call both stdout and stderr are used and merged
 #! this is really needed probably only by Intel which uses stderr, 
 #! but it probably also makes no problem when used everywhere
+    try:
+        output_compiler = subprocess.Popen([compiler,version_flag], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+    except:
+        output_compiler = ''
+    match_compiler = re.search(pattern, output_compiler.decode('utf-8'))
+    if match_compiler:
+        compiler_version = match_compiler.group()
+    else:
+        compiler_version = '0.0'
+    return compiler_version
+
 
 #! looking for C compiler version
 if C_COMPILER_VENDOR == 'GNU':
-    try:
-        outputGCC = subprocess.Popen([C_COMPILER, '--version'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputGCC = ''
-    
-    matchGCC = re.search(r'\d+\.\d+\.\d+', outputGCC)
-    if matchGCC:
-        GCCversion = matchGCC.group()
-    else:
-        GCCversion = '0.0.0'
-
-    C_COMPILER_VERSION = GCCversion
+    C_COMPILER_VERSION = get_compiler_version(C_COMPILER,'--version','\d+\.\d+\.\d+')
 elif C_COMPILER_VENDOR == 'Intel':
-    try:
-        if CMAKE_SYSTEM_NAME == 'Windows':
-            outputIntelC = subprocess.Popen([C_COMPILER, '/V'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-        else:
-            outputIntelC = subprocess.Popen([C_COMPILER, '-V'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputIntelC = ''
-    
-    matchIntelC = re.search(r'\d+\.\d+', outputIntelC)
-    if matchIntelC:
-        IntelCversion = matchIntelC.group()
+    if CMAKE_SYSTEM_NAME == 'Windows':
+        C_COMPILER_VERSION = get_compiler_version(C_COMPILER,'\V','\d+\.\d+')
     else:
-        IntelCversion = '0.0'
-			
-    C_COMPILER_VERSION = IntelCversion
+        C_COMPILER_VERSION = get_compiler_version(C_COMPILER,'-V','\d+\.\d+')
 elif C_COMPILER_VENDOR == 'PGI':
-    try:
-        outputPGIC = subprocess.Popen([C_COMPILER, '-V'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputPGIC = ''
-    
-    matchPGIC = re.search(r'\d+\.\d+', outputPGIC)
-    if matchPGIC:
-        PGICversion = matchPGIC.group()
-    else:
-        PGICversion = '0.0'
-			
-    C_COMPILER_VERSION = PGICversion
+    C_COMPILER_VERSION = get_compiler_version(C_COMPILER,'-V','\d+\.\d+')
 elif C_COMPILER_VENDOR == 'XL':
-    try:
-        outputXLC = subprocess.Popen([C_COMPILER, '-qversion'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputXLC = ''
-    
-    matchXLC = re.search(r'\d+\.\d+', outputXLC)
-    if matchXLC:
-        XLCversion = matchXLC.group()
-    else:
-        XLCversion = '0.0'
-			
-    C_COMPILER_VERSION = XLCversion
+    C_COMPILER_VERSION = get_compiler_version(C_COMPILER,'-qversion','\d+\.\d+')
 elif C_COMPILER_VENDOR == 'Clang':
-    try:
-        outputClangC = subprocess.Popen([C_COMPILER, '--version'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputClangC = ''
-    
-    matchClangC = re.search(r'\d+\.\d+', outputClangC)
-    if matchClangC:
-        ClangCversion = matchClangC.group()
-    else:
-        ClangCversion = '0.0'
-			
-    C_COMPILER_VERSION = ClangCversion
+    C_COMPILER_VERSION =  get_compiler_version(C_COMPILER,'--version','\d+\.\d+')
 else:
     #! unknown, not supported vendor
     C_COMPILER_VERSION = '0.0'
 
 #! looking for C++ compiler version
 if CXX_COMPILER_VENDOR == 'GNU':
-    try:
-        outputGXX = subprocess.Popen([CXX_COMPILER, '--version'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputGXX = ''
-    
-    matchGXX = re.search(r'\d+\.\d+\.\d+', outputGXX)
-    if matchGXX:
-        GXXversion = matchGXX.group()
-    else:
-        GXXversion = '0.0.0'
-
-    CXX_COMPILER_VERSION = GXXversion
+    CXX_COMPILER_VERSION = get_compiler_version(CXX_COMPILER,'--version','\d+\.\d+\.\d+')
 elif CXX_COMPILER_VENDOR == 'Intel':
-    try:
-        if CMAKE_SYSTEM_NAME == 'Windows':
-            outputIntelCXX = subprocess.Popen([CXX_COMPILER, '/V'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-        else:
-            outputIntelCXX = subprocess.Popen([CXX_COMPILER, '-V'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputIntelCXX = ''
-    
-    matchIntelCXX = re.search(r'\d+\.\d+', outputIntelCXX)
-    if matchIntelCXX:
-        IntelCXXversion = matchIntelCXX.group()
+    if CMAKE_SYSTEM_NAME == 'Windows':
+        CXX_COMPILER_VERSION = get_compiler_version(CXX_COMPILER,'/V','\d+\.\d+\.\d+')
     else:
-        IntelCXXversion = '0.0'
-			
-    CXX_COMPILER_VERSION = IntelCXXversion
+        CXX_COMPILER_VERSION = get_compiler_version(CXX_COMPILER,'-V','\d+\.\d+\.\d+')
 elif CXX_COMPILER_VENDOR == 'PGI':
-    try:
-        outputPGICXX = subprocess.Popen([CXX_COMPILER, '-V'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputPGICXX = ''
-    
-    matchPGICXX = re.search(r'\d+\.\d+', outputPGICXX)
-    if matchPGICXX:
-        PGICXXversion = matchPGICXX.group()
-    else:
-        PGICXXversion = '0.0'
-			
-    CXX_COMPILER_VERSION = PGICXXversion
+    CXX_COMPILER_VERSION = get_compiler_version(CXX_COMPILER,'-V','\d+\.\d+')
 elif CXX_COMPILER_VENDOR == 'XL':
-    try:
-        outputXLCXX = subprocess.Popen([CXX_COMPILER, '-qversion'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputXLCXX = ''
-    
-    matchXLCXX = re.search(r'\d+\.\d+', outputXLCXX)
-    if matchXLCXX:
-        XLCXXversion = matchXLCXX.group()
-    else:
-        XLCXXversion = '0.0'
-			
-    CXX_COMPILER_VERSION = XLCXXversion
+    CXX_COMPILER_VERSION = get_compiler_version(CXX_COMPILER,'-qversion','\d+\.\d+')
 elif CXX_COMPILER_VENDOR == 'Clang':
-    try:
-        outputClangCXX = subprocess.Popen([CXX_COMPILER, '--version'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputClangCXX = ''
-    
-    matchClangCXX = re.search(r'\d+\.\d+', outputClangCXX)
-    if matchClangCXX:
-        ClangCXXversion = matchClangCXX.group()
-    else:
-        ClangCXXversion = '0.0'
-			
-    CXX_COMPILER_VERSION = ClangCXXversion
+    CXX_COMPILER_VERSION = get_compiler_version(CXX_COMPILER,'--version','\d+\.\d+')
 else:
     #! unknown, not supported vendor
     CXX_COMPILER_VERSION = '0.0'
 
 #! looking for Fortran compiler version
 if Fortran_COMPILER_VENDOR == 'GNU':
-    try:
-        outputGFORTRAN = subprocess.Popen([Fortran_COMPILER, '--version'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputGFORTRAN = ''
-    
-    matchGFORTRAN = re.search(r'\d+\.\d+\.\d+', outputGFORTRAN)
-    if matchGFORTRAN:
-        GFORTRANversion = matchGFORTRAN.group()
-    else:
-        GFORTRANversion = '0.0.0'
-
-    Fortran_COMPILER_VERSION = GFORTRANversion
+    Fortran_COMPILER_VERSION = get_compiler_version(Fortran_COMPILER,'--version','\d+\.\d+\.\d+')
 elif Fortran_COMPILER_VENDOR == 'Intel':
-    try:
-        if CMAKE_SYSTEM_NAME == 'Windows':
-            outputIntelFORTRAN = subprocess.Popen([Fortran_COMPILER, '/V'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-        else:
-            outputIntelFORTRAN = subprocess.Popen([Fortran_COMPILER, '-V'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputIntelFORTRAN = ''
-    
-    matchIntelFORTRAN = re.search(r'\d+\.\d+', outputIntelFORTRAN)
-    if matchIntelFORTRAN:
-        IntelFORTRANversion = matchIntelFORTRAN.group()
+    if CMAKE_SYSTEM_NAME == 'Windows':
+        Fortran_COMPILER_VERSION = get_compiler_version(Fortran_COMPILER,'/V','\d+\.\d+')
     else:
-        IntelFORTRANversion = '0.0'
-			
-    Fortran_COMPILER_VERSION = IntelFORTRANversion
+        Fortran_COMPILER_VERSION = get_compiler_version(Fortran_COMPILER,'-V','\d+\.\d+')
 elif Fortran_COMPILER_VENDOR == 'PGI':
-    try:
-        outputPGIFORTRAN = subprocess.Popen([Fortran_COMPILER, '-V'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputPGIFORTRAN = ''
-    
-    matchPGIFORTRAN = re.search(r'\d+\.\d+', outputPGIFORTRAN)
-    if matchPGIFORTRAN:
-        PGIFORTRANversion = matchPGIFORTRAN.group()
-    else:
-        PGIFORTRANversion = '0.0'
-			
-    Fortran_COMPILER_VERSION = PGIFORTRANversion
+    Fortran_COMPILER_VERSION = get_compiler_version(Fortran_COMPILER,'-V','\d+\.\d+')
 elif Fortran_COMPILER_VENDOR == 'XL':
-    try:
-        outputXLFORTRAN = subprocess.Popen([Fortran_COMPILER, '-qversion'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    except:
-        outputXLFORTRAN = ''
-    
-    matchXLFORTRAN = re.search(r'\d+\.\d+', outputXLFORTRAN)
-    if matchXLFORTRAN:
-        XLFORTRANversion = matchXLFORTRAN.group()
-    else:
-        XLFORTRANversion = '0.0'
-			
-    Fortran_COMPILER_VERSION = XLFORTRANversion
+    Fortran_COMPILER_VERSION = get_compiler_version(Fortran_COMPILER,'-qversion','\d+\.\d+')
 else:
     #! unknown, not supported vendor
     Fortran_COMPILER_VERSION = '0.0'
 
-
 #! this is the standard output for CMake (must be in this order and with ;)
-print (C_COMPILER_VERSION + ';' + CXX_COMPILER_VERSION + ';' + Fortran_COMPILER_VERSION)
+print(C_COMPILER_VERSION + ';' + CXX_COMPILER_VERSION + ';' + Fortran_COMPILER_VERSION)
